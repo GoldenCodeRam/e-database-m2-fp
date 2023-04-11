b9517a96b69cf65c714ec49129f6c69c054c7a35
Luis Alejandro Quiroga Gómez <lualquigo@gmail.com>
2023-04-11 11:14:24 -0500

    feat(ferret)!: finished scraper
    BREAKING CHANGE: removed colly in favor of ferret

--- cmd/scrapper.go
+++ cmd/scrapper.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"log"
-	"strings"
+	"os"
 
-	"github.com/PuerkitoBio/goquery"
-	"github.com/gocolly/colly"
+	"github.com/MontFerret/ferret/pkg/compiler"
+	"github.com/MontFerret/ferret/pkg/drivers"
+	"github.com/MontFerret/ferret/pkg/drivers/cdp"
 )
 
 const (
@@ -13,71 +17,148 @@ const (
 	ITEM_DESCRIPTION_URL = "www.mercadolibre.com.co"
 )
 
-const (
-	ITEM_A_CLASS = "a.ui-search-item__group__element.shops__items-group-details.ui-search-link"
-	ITEM_TITLE   = "h1.ui-pdp-title"
-)
-
-var links = []string{
-	"https://listado.mercadolibre.com.co/portatil",
+var contentToScrap = []Content{
+	{
+		Type:  "Laptops",
+		Url:   "https://listado.mercadolibre.com.co/portátiles",
+		Items: &[]Item{},
+	},
+	{
+		Type:  "Cameras",
+		Url:   "https://listado.mercadolibre.com.co/cámaras",
+		Items: &[]Item{},
+	},
+	{
+		Type:  "Phones",
+		Url:   "https://listado.mercadolibre.com.co/teléfonos",
+		Items: &[]Item{},
+	},
+	{
+		Type:  "Televisions",
+		Url:   "https://listado.mercadolibre.com.co/televisores",
+		Items: &[]Item{},
+	},
+	{
+		Type:  "Earphones",
+		Url:   "https://listado.mercadolibre.com.co/audífonos",
+		Items: &[]Item{},
+	},
 }
+
+type Content struct {
+	Type  string
+	Url   string
+	Items *[]Item
+}
+
+type Item struct {
+	Name        string   `json:"name"`
+	Price       int      `json:"price"`
+	Images      []string `json:"images"`
+    Reviews     int      `json:"reviews"`
+	Description string   `json:"description"`
+	Url         string
+}
+
+var scrapingResult = []Content{}
 
 func main() {
-	c := colly.NewCollector(
-		colly.AllowedDomains(
-			ITEM_LIST_URL,
-			ITEM_DESCRIPTION_URL,
-		),
-	)
 
-	// parse item list first
-	c.OnResponse(func(r *colly.Response) {
-		switch r.Request.URL.Hostname() {
-		case ITEM_LIST_URL:
-			visitItems(c, r.Body)
-		case ITEM_DESCRIPTION_URL:
-			scrapItem(c, r.Body)
+	log.Println("Starting scraper...")
+	for _, content := range contentToScrap {
+		itemListQuery := fmt.Sprintf(`
+        LET doc = DOCUMENT("%s")
+        LET nextPage = "li.andes-pagination__button.andes-pagination__button--next.shops__pagination-button a"
+        LET itemClass = "a.ui-search-item__group__element.shops__items-group-details.ui-search-link"
+        LET pages = 1
+
+        LET result = (
+            FOR page IN 1..pages
+            LET clicked = page == 1 ? false : CLICK(doc, nextPage)
+            LET wait = clicked ? WAIT_NAVIGATION(doc, 10000) : false
+
+            LET items = (
+                FOR el IN ELEMENTS(doc, itemClass)
+                    RETURN TRIM(el.attributes.href)
+            )
+
+            RETURN items
+        )
+
+        RETURN FLATTEN(result)
+        `, content.Url)
+
+		comp := compiler.New()
+		program, _ := comp.Compile(itemListQuery)
+
+		ctx := context.Background()
+
+		ctx = drivers.WithContext(ctx, cdp.NewDriver(), drivers.AsDefault())
+
+		out, err := program.Run(ctx)
+
+		if err != nil {
+			panic(err)
 		}
-	})
 
-	// startpoint
-	c.Visit("https://listado.mercadolibre.com.co/portatil")
-}
+		var itemUrls = []string{}
 
-func visitItems(c *colly.Collector, body []byte) {
-	queryBody, err := newDocumentFromBody(body)
+		json.Unmarshal(out, &itemUrls)
 
-	if err != nil {
-		panic(err)
+		log.Printf("Items found: %d for category: %s", len(itemUrls), content.Type)
+		log.Println("Starting item scraping...")
+		for i, url := range itemUrls {
+
+			log.Printf("Scraping %d from %d...", i+1, len(itemUrls))
+
+			itemQuery := fmt.Sprintf(`
+            LET doc = DOCUMENT("%s")
+
+            LET mainSelector = "div.ui-pdp-price__second-line span meta"
+            LET nameSelector = "h1.ui-pdp-title"
+            LET priceSelector = "div.ui-pdp-price__second-line span meta"
+            LET imagesSelector = "figure.ui-pdp-gallery__figure img"
+            LET reviewsSelector = "span.ui-pdp-review__amount"
+            LET descriptionSelector = "p.ui-pdp-description__content"
+
+            WAIT_ELEMENT(doc, mainSelector)
+
+            LET images = (
+                FOR image IN ELEMENTS(doc, imagesSelector)
+                RETURN image.attributes.src
+            )
+
+            RETURN {
+                name: ELEMENT(doc, nameSelector).innerText,
+                price: TO_INT(ELEMENT(doc, priceSelector).attributes.content),
+                images: images,
+                reviews: TO_INT(REGEX_MATCH(ELEMENT(doc, reviewsSelector).innerText, "[0-9]+")),
+                description: ELEMENT(doc, descriptionSelector).innerText,
+            }
+            `, url)
+
+			prg, _ := comp.Compile(itemQuery)
+
+			o, _ := prg.Run(ctx)
+
+			item := Item{
+				Url: url,
+			}
+			json.Unmarshal(o, &item)
+
+            *content.Items = append(*content.Items, item)
+		}
 	}
 
-	queryBody.Find(ITEM_A_CLASS).Each(func(i int, s *goquery.Selection) {
-		href, found := s.Attr("href")
+    out, err := json.Marshal(contentToScrap)
 
-		if found {
-			//log.Printf("Found item: %s", href)
-			c.Visit(href)
-		}
-	})
-}
+    if err != nil {
+        panic(err)
+    }
 
-func scrapItem(c *colly.Collector, body []byte) {
-	queryBody, err := newDocumentFromBody(body)
+    err = os.WriteFile("./out/dat.json", out, 0644)
 
-	if err != nil {
-		panic(err)
-	}
-
-	title := queryBody.Find("h1.ui-pdp-title").Text()
-
-	images := 0
-	queryBody.Find("div.ui-pdp-gallery__column").Find("img").Each(func(i int, s *goquery.Selection) {
-		images += 1
-	})
-
-	log.Println(title, images)
-}
-
-func newDocumentFromBody(body []byte) (*goquery.Document, error) {
-	return goquery.NewDocumentFromReader(strings.NewReader(string(body)))
+    if err != nil {
+        panic(err)
+    }
 }
--- go.mod
+++ go.mod
@@ -2,22 +2,22 @@ module github.com/goldencoderam
 
 go 1.20
 
+require github.com/MontFerret/ferret v0.18.0
+
 require (
-	github.com/PuerkitoBio/goquery v1.8.1 // indirect
-	github.com/andybalholm/cascadia v1.3.1 // indirect
-	github.com/antchfx/htmlquery v1.3.0 // indirect
-	github.com/antchfx/xmlquery v1.3.15 // indirect
-	github.com/antchfx/xpath v1.2.4 // indirect
+	github.com/antlr/antlr4/runtime/Go/antlr/v4 v4.0.0-20230321174746-8dcc6526cfb1 // indirect
+	github.com/corpix/uarand v0.2.0 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
-	github.com/gocolly/colly v1.2.0 // indirect
-	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
-	github.com/golang/protobuf v1.5.3 // indirect
-	github.com/jawher/mow.cli v1.2.0 // indirect
-	github.com/kennygrant/sanitize v1.2.4 // indirect
-	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
-	github.com/temoto/robotstxt v1.1.2 // indirect
+	github.com/gorilla/css v1.0.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/mafredri/cdp v0.34.0 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rs/zerolog v1.29.0 // indirect
+	github.com/wI2L/jettison v0.7.4 // indirect
+	golang.org/x/exp v0.0.0-20220722155223-a9213eeb770e // indirect
 	golang.org/x/net v0.9.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
-	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/protobuf v1.30.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
 )
